836ee7028d2a51b133bfbd89116d7ef81d5136a9
Luis Fernando Gonzalez <88568999+lugoz99@users.noreply.github.com>
2026-02-08 12:45:13 -0500

    Delete src directory

--- src/auth/interfaces/auth.response.ts
+++
@@ -1,6 +0,0 @@
-import type { User } from "@/types/user.interface";
-// Login || register || checkstatus -> sirve para estos
-export interface AuthResponse{
-  user:User
-  token:string;
-}
--- src/lib/sleep.ts
+++
@@ -1,3 +0,0 @@
-export const sleep = (ms: number = 1000) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
